896976342d9247cd097d188a1424e45ffe856e90
Robert Hovey <robert.hovey95@gmail.com>
2023-02-16 07:33:03 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Robert Alec Hovey on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
